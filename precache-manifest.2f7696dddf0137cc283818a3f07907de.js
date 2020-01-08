6f5f2b61178ac5bb141f1523fe31b1280e403cb7
Okoro Nnamdi <Nnasp4christ@yahoo.com>
2020-01-08 17:21:53 +0100

    Updates

---
+++ precache-manifest.2f7696dddf0137cc283818a3f07907de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e8904c6bdb58974353b12f2f43de73",
+    "url": "/React-on-todo-app-in-github/index.html"
+  },
+  {
+    "revision": "5ed8f3f73b5081c2cfe6",
+    "url": "/React-on-todo-app-in-github/static/css/main.b9420fb3.chunk.css"
+  },
+  {
+    "revision": "2ad552faa2d11e6b55b7",
+    "url": "/React-on-todo-app-in-github/static/js/2.7f9f5bc7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-on-todo-app-in-github/static/js/2.7f9f5bc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ed8f3f73b5081c2cfe6",
+    "url": "/React-on-todo-app-in-github/static/js/main.c5b62007.chunk.js"
+  },
+  {
+    "revision": "00fad357f235b97639e0",
+    "url": "/React-on-todo-app-in-github/static/js/runtime-main.e16d648f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-on-todo-app-in-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
